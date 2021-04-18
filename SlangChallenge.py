d4f4af67e0781bc9d8d7c62d6b41649689288e72
SantiagoE <s.estupinan@uniandes.edu.co>
2021-04-18 15:31:38 -0500

    Checked edge cases (if n > length of text)

--- SlangChallenge.py
+++ SlangChallenge.py
@@ -2,10 +2,14 @@ def calculateNGrams(text, n):
     n = int(n)
     answer = []
     text_len = len(text)
+    if n >= text_len:
+        answer.append(text)
+        print(answer)
+        return
     for i in range(text_len):
         if i+n <= text_len:
             answer.append(text[i:i+n])
-    print(str(answer))
+    print(answer)
 
 
 calculateNGrams(input("Text used for the n-gram:"), input("Number of n items:"))
