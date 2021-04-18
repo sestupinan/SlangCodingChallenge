240629dd237f242c0a0c25fbd8d213daec4fc01b
SantiagoE <s.estupinan@uniandes.edu.co>
2021-04-18 15:57:07 -0500

    Added menu where you can shoose the step to perform. Added structure for Step#2(mostFrequentNGram)

--- SlangChallenge.py
+++ SlangChallenge.py
@@ -1,3 +1,4 @@
+
 def calculateNGrams(text, n):
     n = int(n)
     answer = []
@@ -16,4 +17,30 @@ def calculateNGrams(text, n):
     # total complexity O(text_len * n)
     print(answer)
 
-calculateNGrams(input("Text used for the n-gram:"), input("Number of n items:"))
+def mostFrequentNGRam(text, n):
+    n = int(n)
+    answer = []
+    text_len = len(text)
+    # we check for edge cases (if n is bigger than the length of the text, we return the text itself)
+    if n >= text_len:
+        answer.append(text)
+        print(answer)
+        return
+    # we use python slicing to substring the text to calculate its n-grams.
+    # we iterate over the text (O(text_len))
+    for i in range(text_len):
+        if i+n <= text_len:
+            # we slice and append the n-gram in the array (worst case scenario O(n))
+            answer.append(text[i:i+n])
+    # total complexity O(text_len * n)
+    print(answer)
+
+def menu():
+    step = input("Select the step you want to run:\nType '1' for Step 1 and '2' for Step 2\n")
+    if step == "1":
+        calculateNGrams(input("Text used for the n-gram:"), input("Number of n items:"))
+    elif step == "2":
+        mostFrequentNGRam()
+    else:
+        print("Pick a valid step.")
+menu()
