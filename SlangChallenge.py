a05503ba8435672714c05699995eb5e50eff8800
SantiagoE <s.estupinan@uniandes.edu.co>
2021-04-18 15:47:56 -0500

    First step documented

--- SlangChallenge.py
+++ SlangChallenge.py
@@ -2,14 +2,18 @@ def calculateNGrams(text, n):
     n = int(n)
     answer = []
     text_len = len(text)
+    # we check for edge cases (if n is bigger than the length of the text, we return the text itself)
     if n >= text_len:
         answer.append(text)
         print(answer)
         return
+    # we use python slicing to substring the text to calculate its n-grams.
+    # we iterate over the text (O(text_len))
     for i in range(text_len):
         if i+n <= text_len:
+            # we slice and append the n-gram in the array (worst case scenario O(n))
             answer.append(text[i:i+n])
+    # total complexity O(text_len * n)
     print(answer)
-
 
 calculateNGrams(input("Text used for the n-gram:"), input("Number of n items:"))
